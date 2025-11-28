ef036c499f4abcc0fdebef1c71a92b493ec1b091
stinahaziri <stinna.haziri@gmail.com>
2025-11-28 18:49:26 +0100

    z

---
+++ frontend/src/pages/addTask.tsx
@@ -0,0 +1,66 @@
+import "./styles/signUpStyle.css";
+
+import Header from "./header"
+
+function AddTask() {
+  return (
+    <>
+    <Header/>
+
+    <section className="signUp">
+      <div className="title">
+        <h1>Add Task</h1>
+      </div>
+      <div className="colum">
+        <form>
+          <div className="firstInput">
+            <div className="inputGroup">
+              <input type="text" placeholder="Title" />
+            </div>
+            <div className="inputGroup">
+              <input
+                type="datetime-local"
+                placeholder="Deadline "
+                // maxlength="8"
+              />
+            </div>
+          </div>
+          <div className="seconInput">
+            {/* <div className="inputGroup">
+            <input type="text" placeholder="Email" maxlength="8" />
+          </div> */}
+            {/* <div className="inputGroup">
+            <input type="" placeholder="Phone" maxlength="8" />
+          </div> */}
+            <div className="inputGroup">
+              <textarea
+                name=""
+                id=""
+                // cols="55"
+                // rows="10"
+                placeholder="Comentt"
+              ></textarea>
+            </div>
+            <div className="inputGroup">
+              <select>
+                <option value="someOption">Some option</option>
+                <option value="otherOption">Other option</option>
+              </select>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div className="create">
+        {/* <h4>Terms of service:</h4>
+      <input name="terms" className="agree" type="radio" />Agree
+      <input name="terms" className="disagree" type="radio" />Disagree */}
+        <a href="getDemo.html">
+          <input type="submit" />
+        </a>
+      </div>
+    </section>
+        </>
+  );
+}
+export default AddTask;
---
+++ frontend/src/pages/header.tsx
@@ -0,0 +1,30 @@
+
+import './styles/getDemoStyle.css';
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <a href="index.html">
+          <img className="logo" src="image/Capture-removebg-preview.png" alt="" />
+        </a>
+      </div>
+      
+      <input type="checkbox" id="check"/>
+      <label className="checkInput">
+        <i className="fa-solid fa-bars"></i>
+      </label>
+      
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="news1.html">News</a></li>
+        <li><a href="pricing.html">Pricing</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+      <img className="acc" src="image/HKstrategies-755-1-1024x683.jpg" alt="" />
+    </header>
+  );
+}
+
+export default Header;
