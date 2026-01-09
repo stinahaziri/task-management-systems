d68acf6f62a61a4898ff59f595cccda2f24040ad
doreartabalaj <db70730@ubt-uni.net>
2026-01-09 22:26:52 +0100

    0

--- backend/Controllers/SystemConfiguration.cs
+++
@@ -1,7 +0,0 @@
-public DbSet<SystemConfiguration> SystemConfigurations { get; set; }
-public class SystemConfiguration
-{
-    public int ID { get; set; }
-    public int MaxTasksPerUser { get; set; }
-    public int DefaultPriority { get; set; }
-}
